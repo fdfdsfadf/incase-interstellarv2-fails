41eddb25f36cba85f73d1aac1afa54600d9b9901
fdfdsfadf <Hu_Gaming1@outlook.com>
2025-08-08 18:44:18 +1000

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,9 @@ const config = {
   challenge: true, // Set to true if you want to enable password protection.
   users: {
     // You can add multiple users by doing username: 'password'.
-    interstellar: "yaydiscord",
+    Hugo: "yaydiscord",
+    Alerio: "Alerio123#",
+    Judah: "BrokenHand",
   },
 };
 
